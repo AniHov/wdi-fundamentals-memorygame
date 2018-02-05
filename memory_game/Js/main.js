7b1d8271c72091907ba8012d34985b9d30a42c88
AniHov <35086521+AniHov@users.noreply.github.com>
2018-02-04 22:21:59 -0800

    created game logic

--- memory_game/Js/main.js
+++ memory_game/Js/main.js
@@ -1,6 +1,29 @@
 
 
-var cards = ["My Girl", "My Girl", "My Man", "My Man"];
+var cards = [
+{
+  rank: "Queen",
+  suit: "Hearts",
+  cardImage: "images/queen-of-hearts.png"
+},
+{
+  rank: "Queen",
+  suit: "Diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+},
+{
+  rank: "King",
+  suit: "Hearts",
+  cardImage: "images/king-of-hearts.png"
+},
+{
+  rank: "King",
+  suit: "Diamonds",
+  cardImage: "images/king-of-diamonds.png"
+}
+];
+
+
 var cardsInPlay = [];
 
 var checkForMatch = function (){
@@ -12,9 +35,11 @@ console.log("Seriously?!? Try again.");
 };
 
 var flipCard = function (cardId){
-console.log("You just flipped "+ cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("You just flipped "+ cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
 checkForMatch();
+console.log('suit: '+ cards[cardId].suit);
+console.log('pic: ' +cards[cardId].cardImage);
 };
 
 flipCard(0);
